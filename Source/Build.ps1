e4b7a8caabb8efed66b41e714a91ad2cc080cc4b
Stephen Cleary <liveid.20.stephen-cleary@spamgourmet.com>
2014-05-09 18:03:40 -0400

    Moved to new PCL.

--- Source/Build.ps1
+++ Source/Build.ps1
@@ -8,6 +8,6 @@ $build.SolutionConfigurations.Item("Release").Activate()
 $build.Build($true)
 $oldConfiguration.Activate()
 
-nuget pack -Symbols "Comparers\Comparers (NET4).csproj" -Prop Configuration=Release
+nuget pack -Symbols "Comparers (NET4, Win8, SL5, WP8, WPA)\Comparers (NET4, Win8, SL5, WP8, WPA).csproj" -Prop Configuration=Release
 nuget pack -Symbols "Comparers.Ix (NET45, Win8, WP8)\Comparers.Ix (NET45, Win8, WP8).csproj" -Prop Configuration=Release
 nuget pack -Symbols "Comparers.Rx (NET4, Win8, SL5, WP8)\Comparers.Rx (NET4, Win8, SL5, WP8).csproj" -Prop Configuration=Release
