4ec59cfacebf14e698b2e81ebf60447890863be3
Stephen Cleary <liveid.20.stephen-cleary@spamgourmet.com>
2014-05-09 21:33:57 -0400

    Added non-generic IFullComparer.

---
+++ Source/Comparers (NET4, Win8, SL5, WP8, WPA)/Comparers/IFullComparer (of T).cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Comparers
+{
+    /// <summary>
+    /// A comparer that also provides equality comparison (and hash codes).
+    /// </summary>
+    /// <typeparam name="T">The type of objects being compared.</typeparam>
+    public interface IFullComparer<in T> : IComparer<T>, IEqualityComparer<T>, IFullComparer
+    {
+    }
+}
--- Source/Comparers (NET4, Win8, SL5, WP8, WPA)/Comparers/IFullComparer.cs
+++ Source/Comparers (NET4, Win8, SL5, WP8, WPA)/Comparers/IFullComparer.cs
@@ -6,14 +6,9 @@ using System.Text;
 namespace Comparers
 {
     /// <summary>
-    /// A comparer that also provides equality comparison (and hash codes).
+    /// A non-generic comparer that also provides equality comparison (and hash codes).
     /// </summary>
-    /// <typeparam name="T">The type of objects being compared.</typeparam>
-#if NO_GENERIC_VARIANCE
-    public interface IFullComparer<T> : IComparer<T>, IEqualityComparer<T>, System.Collections.IComparer, System.Collections.IEqualityComparer
-#else
-    public interface IFullComparer<in T> : IComparer<T>, IEqualityComparer<T>, System.Collections.IComparer, System.Collections.IEqualityComparer
-#endif
+    public interface IFullComparer : System.Collections.IComparer, System.Collections.IEqualityComparer
     {
     }
 }
