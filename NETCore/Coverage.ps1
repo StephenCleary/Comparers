62da9f674a0987f87ec523c5bd3c5aae689a45e5
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-04-19 20:17:32 -0400

    Upgrade to VS2015.

--- NETCore/Coverage.ps1
+++ NETCore/Coverage.ps1
@@ -1,2 +1,4 @@
 $artifactLocation = 'artifacts\bin\Nito.Comparers\Debug\net45'
-iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/master/Coverage.ps1'))
+$testProjectLocation = 'test/UnitTests'
+$outputLocation = 'testResults'
+iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/bb636fc76a9017d3cee13d7229539007077845ef/Coverage.ps1'))
