a209189de1d9d2298fb6a5fa7e342edfc7169175
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-06-28 21:33:57 -0400

    Update to NetCore RTM.

--- NETCore/Coverage.ps1
+++ NETCore/Coverage.ps1
@@ -1,3 +1,3 @@
 $testProjectLocations = @('test/UnitTests')
 $outputLocation = 'testResults'
-iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/640c77d28ced405458b067de901bc611db60b880/Coverage.ps1'))
+iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/599beba35b53f495d4df6e5c323573aa839137a3/Coverage.ps1'))
