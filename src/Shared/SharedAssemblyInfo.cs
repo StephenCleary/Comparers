7f878c4cd86c103320bf29489c8324e99bca51f6
Stephen Cleary <github.ourteddybear@xoxy.net>
2017-01-28 17:30:59 -0500

    Add shared assembly info.

---
+++ src/Shared/SharedAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Stephen Cleary")]
+[assembly: AssemblyProduct("Nito.Comparers")]
+
+[assembly: AssemblyVersion("5.0.0")]
+[assembly: AssemblyFileVersion("5.0.0")]
+[assembly: AssemblyInformationalVersion("5.0.0-pre-03")]
