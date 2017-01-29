b2402f1155c73b7afe64c7cbf13085e580584f56
Stephen Cleary <github.ourteddybear@xoxy.net>
2017-01-28 19:44:02 -0500

    Fix bug in version script.

--- Version.ps1
+++ Version.ps1
@@ -1,2 +1,2 @@
 param([String]$oldVersion="", [String]$newVersion="")
-iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/18aed44c432381eaa11329d82f63dfaf835d219c/Version.ps1'))
+iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/0297d9e34cc1ff5391b474b4fa29d6be9b236b83/Version.ps1'))
