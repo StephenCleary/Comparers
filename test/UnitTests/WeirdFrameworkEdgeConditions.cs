7e4a7a512fa0c49babe9836db0f75bc3b9bfdd5b
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-02-05 11:24:06 -0500

    Add doc reference.

--- test/UnitTests/WeirdFrameworkEdgeConditions.cs
+++ test/UnitTests/WeirdFrameworkEdgeConditions.cs
@@ -18,17 +18,17 @@ namespace UnitTests
             IEqualityComparer nitoComparer = ComparerBuilder.For<int>().Default();
             var obj1 = new object();
 
-            // The framework version returns true if any reference-equal instances are passed in...
+            // The framework version returns true if any reference-equal instances are passed in
             //   https://github.com/dotnet/corefx/blob/53a33cf2662ac8c9a45d13067012d80cf0ba6956/src/Common/src/CoreLib/System/Collections/Generic/EqualityComparer.cs#L29
             Assert.True(frameworkComparer.Equals(obj1, obj1));
             
-            // But throws if that same object is passed to GetHashCode.
+            // But throws if that same object is passed to GetHashCode
             Assert.ThrowsAny<ArgumentException>(() => frameworkComparer.GetHashCode(obj1));
 
-            // It also throws if different instances are passed in that are not the expected type.
+            // It also throws if different instances are passed in that are not the expected type
             Assert.ThrowsAny<ArgumentException>(() => frameworkComparer.Equals(obj1, new object()));
 
-            // Nito Comparers throw in all scenarios for consistency.
+            // Nito Comparers throw in all scenarios for consistency
             Assert.ThrowsAny<ArgumentException>(() => nitoComparer.Equals(obj1, obj1));
             Assert.ThrowsAny<ArgumentException>(() => nitoComparer.GetHashCode(obj1));
             Assert.ThrowsAny<ArgumentException>(() => nitoComparer.Equals(obj1, new object()));
@@ -45,10 +45,11 @@ namespace UnitTests
             Assert.True(frameworkStringComparer.Equals(null, null));
             Assert.False(frameworkStringComparer.Equals("test", null));
 
-            // But throws if null is passed to GetHashCode.
+            // But throws if null is passed to GetHashCode
+            //   https://github.com/dotnet/corefx/blob/36e812b0e974fa113c50be71e1701735a8a63481/src/Common/src/CoreLib/System/StringComparer.cs#L150-L153
             Assert.ThrowsAny<ArgumentException>(() => frameworkStringComparer.GetHashCode(null));
 
-            // The framework default comparer does not throw.
+            // The framework default comparer does not throw
             Assert.True(frameworkDefaultComparer.Equals(null, null));
             Assert.False(frameworkDefaultComparer.Equals("test", null));
             frameworkDefaultComparer.GetHashCode(null);
