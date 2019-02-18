b789fd4f43b3ab5c9839cdf072015206d854f12e
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-02-18 16:26:48 -0500

    More rx busywork unit tests.

--- test/Rx.UnitTests/RxEnumerableExtensions.cs
+++ test/Rx.UnitTests/RxEnumerableExtensions.cs
@@ -12,11 +12,75 @@ namespace Rx.UnitTests
     public class RxEnumerableExtensionsUnitTests
     {
         [Fact]
-        public async Task Max_OrdersByComparer()
+        public async Task Max_UsesComparer()
         {
             var input = new int?[] { int.MaxValue, int.MinValue, null, 0, 1, 2, 3 }.ToObservable();
             var result = await input.Max(c => c.OrderBy(x => x % 2 == 0).ThenBy(x => x)).LastAsync();
             Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public async Task Max_ResultSelector_UsesComparer()
+        {
+            var input = new int?[] { int.MaxValue, int.MinValue, null, 0, 1, 2, 3 }.ToObservable();
+            var result = await input.Max(x => x, c => c.OrderBy(x => x % 2 == 0).ThenBy(x => x)).LastAsync();
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public async Task MaxBy_UsesComparer()
+        {
+            var input = new int?[] { int.MaxValue, int.MinValue, null, 0, 1, 2, 3 }.ToObservable();
+            var result = await input.MaxBy(x => x, c => c.OrderBy(x => x % 2 == 0).ThenBy(x => x)).LastAsync();
+            Assert.Equal(new int?[] { 2 }, result);
+        }
+
+        [Fact]
+        public async Task Min_UsesComparer()
+        {
+            var input = new int?[] { int.MaxValue, int.MinValue, null, 0, 1, 2, 3 }.ToObservable();
+            var result = await input.Min(c => c.OrderBy(x => x % 2 == 0).ThenBy(x => x)).LastAsync();
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public async Task Min_ResultSelector_UsesComparer()
+        {
+            var input = new int?[] { int.MaxValue, int.MinValue, null, 0, 1, 2, 3 }.ToObservable();
+            var result = await input.Min(x => x, c => c.OrderBy(x => x % 2 == 0).ThenBy(x => x)).LastAsync();
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public async Task MinBy_UsesComparer()
+        {
+            var input = new int?[] { int.MaxValue, int.MinValue, null, 0, 1, 2, 3 }.ToObservable();
+            var result = await input.MinBy(x => x, c => c.OrderBy(x => x % 2 == 0).ThenBy(x => x)).LastAsync();
+            Assert.Equal(new int?[] { null }, result);
+        }
+
+        [Fact]
+        public async Task Contains_UsesComparer()
+        {
+            var input = new int[] { 0, 1, 2 }.ToObservable();
+            var result = await input.Contains(5, c => c.EquateBy(x => x % 3)).LastAsync();
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task Distinct_UsesComparer()
+        {
+            var input = new int[] { 0, 1, 2, 3, 4, 5 }.ToObservable();
+            var result = await input.Distinct(c => c.EquateBy(x => x % 3)).ToList().LastAsync();
+            Assert.Equal(new[] { 0, 1, 2 }, result);
+        }
+
+        [Fact]
+        public async Task Distinct_KeySelector_UsesComparer()
+        {
+            var input = new int[] { 0, 1, 2, 3, 4, 5 }.ToObservable();
+            var result = await input.Distinct(x => x, c => c.EquateBy(x => x % 3)).ToList().LastAsync();
+            Assert.Equal(new[] { 0, 1, 2 }, result);
         }
     }
 }
