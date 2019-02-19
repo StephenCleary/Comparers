67e7d6d7bcbf60607e3ea0f4c03d9cbfc24a4688
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-02-18 20:52:13 -0500

    Remaining rx busywork unit tests.

--- test/Rx.UnitTests/RxEnumerableExtensions.cs
+++ test/Rx.UnitTests/RxEnumerableExtensions.cs
@@ -6,6 +6,7 @@ using System;
 using Nito.Comparers;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
+using System.Reactive;
 
 namespace Rx.UnitTests
 {
@@ -141,6 +142,104 @@ namespace Rx.UnitTests
             Assert.Equal(new[] { 1, 4 }, result[0]);
             Assert.Equal(new[] { 2, 5 }, result[1]);
             Assert.Equal(new[] { 3 }, result[2]);
+        }
+
+        [Fact]
+        public async Task GroupByUntil_UsesComparer()
+        {
+            var values = new int[] { 1, 2, 3, 4, 5 }.ToObservable();
+            var result = await values.GroupByUntil(x => x, x => Observable.Never<Unit>(), c => c.EquateBy(x => x % 3)).SelectMany(async x => await x.ToList().LastAsync()).ToList().LastAsync();
+            Assert.Equal(3, result.Count);
+            Assert.Contains(new[] { 1, 4 }, result);
+            Assert.Contains(new[] { 2, 5 }, result);
+            Assert.Contains(new[] { 3 }, result);
+        }
+
+        [Fact]
+        public async Task GroupByUntil_ElementSelector_UsesComparer()
+        {
+            var values = new int[] { 1, 2, 3, 4, 5 }.ToObservable();
+            var result = await values.GroupByUntil(x => x, x => x, x => Observable.Never<Unit>(), c => c.EquateBy(x => x % 3)).SelectMany(async x => await x.ToList().LastAsync()).ToList().LastAsync();
+            Assert.Equal(3, result.Count);
+            Assert.Contains(new[] { 1, 4 }, result);
+            Assert.Contains(new[] { 2, 5 }, result);
+            Assert.Contains(new[] { 3 }, result);
+        }
+
+        [Fact]
+        public async Task GroupByUntil_Capacity_UsesComparer()
+        {
+            var values = new int[] { 1, 2, 3, 4, 5 }.ToObservable();
+            var result = await values.GroupByUntil(x => x, x => Observable.Never<Unit>(), 10, c => c.EquateBy(x => x % 3)).SelectMany(async x => await x.ToList().LastAsync()).ToList().LastAsync();
+            Assert.Equal(3, result.Count);
+            Assert.Contains(new[] { 1, 4 }, result);
+            Assert.Contains(new[] { 2, 5 }, result);
+            Assert.Contains(new[] { 3 }, result);
+        }
+
+        [Fact]
+        public async Task GroupByUntil_ElementSelectorAndCapacity_UsesComparer()
+        {
+            var values = new int[] { 1, 2, 3, 4, 5 }.ToObservable();
+            var result = await values.GroupByUntil(x => x, x => x, x => Observable.Never<Unit>(), 10, c => c.EquateBy(x => x % 3)).SelectMany(async x => await x.ToList().LastAsync()).ToList().LastAsync();
+            Assert.Equal(3, result.Count);
+            Assert.Contains(new[] { 1, 4 }, result);
+            Assert.Contains(new[] { 2, 5 }, result);
+            Assert.Contains(new[] { 3 }, result);
+        }
+
+        [Fact]
+        public async Task SequenceEqual_UsesComparer()
+        {
+            var first = new int[] { 0, 1, 2 }.ToObservable();
+            var second = new int[] { 15, 4, 2 }.ToObservable();
+            var result = await first.SequenceEqual(second, c => c.EquateBy(x => x % 3)).LastAsync();
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task SequenceEqual_Enumerable_UsesComparer()
+        {
+            var first = new int[] { 0, 1, 2 }.ToObservable();
+            var second = new int[] { 15, 4, 2 };
+            var result = await first.SequenceEqual(second, c => c.EquateBy(x => x % 3)).LastAsync();
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task ToDictionary_UsesComparer()
+        {
+            var values = new int[] { 1, 2, 3, 4 }.ToObservable();
+            await Assert.ThrowsAnyAsync<ArgumentException>(async () => await values.ToDictionary(x => x, c => c.EquateBy(x => x % 3)).LastAsync());
+        }
+
+        [Fact]
+        public async Task ToDictionary_ElementSelector_UsesComparer()
+        {
+            var values = new int[] { 1, 2, 3, 4 }.ToObservable();
+            await Assert.ThrowsAnyAsync<ArgumentException>(async () => await values.ToDictionary(x => x, x => x, c => c.EquateBy(x => x % 3)).LastAsync());
+        }
+
+        [Fact]
+        public async Task ToLookup_UsesComparer()
+        {
+            var values = new int[] { 1, 2, 3, 4 }.ToObservable();
+            var observableResult = await values.ToLookup(x => x, c => c.EquateBy(x => x % 3)).SelectMany(x => x).ToList().LastAsync();
+            var result = observableResult.Select(x => x.AsEnumerable()).ToList();
+            Assert.Contains(new[] { 3 }, result);
+            Assert.Contains(new[] { 1, 4 }, result);
+            Assert.Contains(new[] { 2 }, result);
+        }
+
+        [Fact]
+        public async Task ToLookup_ElementSelector_UsesComparer()
+        {
+            var values = new int[] { 1, 2, 3, 4 }.ToObservable();
+            var observableResult = await values.ToLookup(x => x, x => x, c => c.EquateBy(x => x % 3)).SelectMany(x => x).ToList().LastAsync();
+            var result = observableResult.Select(x => x.AsEnumerable()).ToList();
+            Assert.Contains(new[] { 3 }, result);
+            Assert.Contains(new[] { 1, 4 }, result);
+            Assert.Contains(new[] { 2 }, result);
         }
     }
 }
