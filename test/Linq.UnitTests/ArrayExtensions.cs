17e3525d36402f1de02b7a1b1a81e1734954cdab
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-02-19 21:43:44 -0500

    Unit tests for list/array sort.

---
+++ test/Linq.UnitTests/ArrayExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Nito.Comparers.Linq;
+using Xunit;
+using System;
+using Nito.Comparers;
+
+namespace Linq.UnitTests
+{
+    public class ArrayExtensionsUnitTests
+    {
+        [Fact]
+        public void Sort_UsesComparer()
+        {
+            var input = new int?[] { int.MaxValue, int.MinValue, null, 0, 1, 2, 3 };
+            input.Sort(c => c.OrderBy(x => x % 2 == 0).ThenBy(x => x));
+            Assert.Equal(new int?[] { null, 1, 3, int.MaxValue, int.MinValue, 0, 2 }, input);
+        }
+
+        [Fact]
+        public void Sort_IndexAndCount_UsesComparer()
+        {
+            var input = new int?[] { int.MaxValue, int.MinValue, null, 0, 3, 2, 1 };
+            input.Sort(2, 4, c => c.OrderBy(x => x % 2 == 0).ThenBy(x => x));
+            Assert.Equal(new int?[] { int.MaxValue, int.MinValue, null, 3, 0, 2, 1 }, input);
+        }
+
+        [Fact]
+        public void Sort_Items_UsesComparer()
+        {
+            var input = new int?[] { int.MaxValue, int.MinValue, null, 0, 1, 2, 3 };
+            var items = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+            input.Sort(items, c => c.OrderBy(x => x % 2 == 0).ThenBy(x => x));
+            Assert.Equal(new int?[] { null, 1, 3, int.MaxValue, int.MinValue, 0, 2 }, input);
+            Assert.Equal(new int[] { 2, 4, 6, 0, 1, 3, 5 }, items);
+        }
+
+        [Fact]
+        public void Sort_Items_IndexAndCount_UsesComparer()
+        {
+            var input = new int?[] { int.MaxValue, int.MinValue, null, 0, 3, 2, 1 };
+            var items = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+            input.Sort(items, 2, 4, c => c.OrderBy(x => x % 2 == 0).ThenBy(x => x));
+            Assert.Equal(new int?[] { int.MaxValue, int.MinValue, null, 3, 0, 2, 1 }, input);
+            Assert.Equal(new int[] { 0, 1, 2, 4, 3, 5, 6 }, items);
+        }
+    }
+}
---
+++ test/Linq.UnitTests/ListExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Nito.Comparers.Linq;
+using Xunit;
+using System;
+using Nito.Comparers;
+
+namespace Linq.UnitTests
+{
+    public class ListExtensionsUnitTests
+    {
+        [Fact]
+        public void Sort_UsesComparer()
+        {
+            var input = new List<int?> { int.MaxValue, int.MinValue, null, 0, 1, 2, 3 };
+            input.Sort(c => c.OrderBy(x => x % 2 == 0).ThenBy(x => x));
+            Assert.Equal(new int?[] { null, 1, 3, int.MaxValue, int.MinValue, 0, 2 }, input);
+        }
+
+        [Fact]
+        public void Sort_IndexAndCount_UsesComparer()
+        {
+            var input = new List<int?> { int.MaxValue, int.MinValue, null, 0, 3, 2, 1 };
+            input.Sort(2, 4, c => c.OrderBy(x => x % 2 == 0).ThenBy(x => x));
+            Assert.Equal(new int?[] { int.MaxValue, int.MinValue, null, 3, 0, 2, 1 }, input);
+        }
+    }
+}
