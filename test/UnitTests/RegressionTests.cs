b2dc1d35c6e526f9f297504de42ee8ace346e37e
Stephen Cleary <github.ourteddybear@xoxy.net>
2020-10-02 21:34:24 -0400

    Add unit tests for #34.

---
+++ test/UnitTests/RegressionTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nito.Comparers;
+using Xunit;
+
+namespace UnitTests
+{
+    public class RegressionTests
+    {
+        [Fact]
+        public void ComparableBaseDefaultComparer_WithoutTypeInstantiation_IsNotNull()
+        {
+            Assert.NotNull(DoNotInstantiateThisType.DefaultComparer);
+        }
+
+        [Fact]
+        public void EquatableBaseDefaultComparer_WithoutTypeInstantiation_IsNotNull()
+        {
+            Assert.NotNull(DoNotInstantiateThisTypeEither.DefaultComparer);
+        }
+
+        private sealed class DoNotInstantiateThisType : ComparableBase<DoNotInstantiateThisType>
+        {
+            static DoNotInstantiateThisType() => DefaultComparer = ComparerBuilder.For<DoNotInstantiateThisType>().Null();
+        }
+
+        private sealed class DoNotInstantiateThisTypeEither : EquatableBase<DoNotInstantiateThisTypeEither>
+        {
+            static DoNotInstantiateThisTypeEither() => DefaultComparer = EqualityComparerBuilder.For<DoNotInstantiateThisTypeEither>().Null();
+        }
+    }
+}
